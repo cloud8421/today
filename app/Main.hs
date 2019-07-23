53c32b89b9d0fa6798c764f44d1c2ef8f212e153
Claudio Ortolina <claudio@pspdfkit.com>
2019-07-23 17:32:59 +0300

    Creates empty Taskfile

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,11 @@ module Main where
 import Lib
 import System.Console.ANSI
 
+defaultTaskFilePath :: FilePath
+defaultTaskFilePath = "./tasks.json"
+
 main :: IO ()
 main = do
+  createTaskFile defaultTaskFilePath
   setSGR [SetColor Foreground Vivid Red]
-  someFunc
   setSGR [Reset]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
 module Lib
-    ( someFunc
-    ) where
+  ( createTaskFile
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Text
+
+data Status
+  = Pending
+  | Progress
+  | Done
+  | Cancelled
+  deriving (Read, Show, Eq)
+
+data Task =
+  Task
+    { status :: Status
+    , text :: Text
+    }
+  deriving (Read, Show, Eq)
+
+createTaskFile :: FilePath -> IO ()
+createTaskFile path = writeFile path "[]"
