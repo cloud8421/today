3e68ad21498139854134db7e4e84269f5503ca54
Kris Jenkins <krisajenkins@gmail.com>
2019-08-08 17:54:09 +0100

    Promoting some bindings from a where clause to top-level.
    Subjective, but I think they deserve it.

--- src/Lib.hs
+++ src/Lib.hs
@@ -50,139 +50,155 @@ optsParser = info (helper <*> programOptions) description
       Opts <$> taskFilePathOption <*>
       (hsubparser taskManagementCommands <|> hsubparser reporterCommands <|>
        hsubparser refManagementCommands)
-    taskManagementCommands :: Mod CommandFields SubCommand
-    taskManagementCommands =
-      commandGroup "Task management:" <> listTasksCommand <> addTaskCommand <>
-      deleteTaskCommand <>
-      checkTaskCommand <>
-      cancelTaskCommand <>
-      startTaskCommand <>
-      pauseTaskCommand <>
-      updateTaskTextCommand <>
-      moveTaskCommand <>
-      clearCommand
-    reporterCommands :: Mod CommandFields SubCommand
-    reporterCommands =
-      commandGroup "Reporters:" <> hidden <> todayCommand <> outForTodayCommand
-    refManagementCommands :: Mod CommandFields SubCommand
-    refManagementCommands =
-      commandGroup "Refs management:" <> hidden <> listRefsCommand <>
-      addRefCommand <>
-      deleteRefCommand
-    textArgument :: Mod ArgumentFields String -> Parser Text
-    textArgument = fmap pack . strArgument
-    textOption :: Mod OptionFields String -> Parser Text
-    textOption = fmap pack . strOption
-    maybeTextOption :: Mod OptionFields (Maybe Text) -> Parser (Maybe Text)
-    maybeTextOption = option maybeText
-      where
-        maybeText = eitherReader (Right . Just . pack)
-    taskFilePathOption :: Parser FilePath
-    taskFilePathOption =
-      strOption
-        (long "taskfile" <> short 'f' <> metavar "TASKFILE" <>
-         value Taskfile.defaultPath <>
-         showDefault <>
-         help Help.taskfile)
-    taskContextOption :: Parser Text
-    taskContextOption =
-      textOption
-        (long "context" <> short 'c' <> value Tasks.defaultContext <>
-         help Help.contextFilter)
-    contextFilterOption :: Parser Ui.ContextFilter
-    contextFilterOption =
-      includeContextFilterOption <|> excludeContextFilterOption
-      where
-        includeContextFilterOption =
-          option
-            includeReader
-            (long "include-context" <> short 'i' <> value Ui.All <>
-             help Help.includeContextFilter)
-        includeReader = eitherReader (Right . Ui.Include . pack)
-        excludeContextFilterOption =
-          option
-            excludeReader
-            (long "exclude-context" <> short 'e' <> value Ui.All <>
-             help Help.excludeContextFilter)
-        excludeReader = eitherReader (Right . Ui.Exclude . pack)
-    taskIdArgument :: Parser Int
-    taskIdArgument = argument auto (help Help.taskId)
-    listTasksCommand :: Mod CommandFields SubCommand
-    listTasksCommand =
-      command "list" (info listTasksOptions (progDesc Help.listTasks))
-    listTasksOptions :: Parser SubCommand
-    listTasksOptions = ListTasks <$> contextFilterOption
-    addTaskCommand :: Mod CommandFields SubCommand
-    addTaskCommand = command "add" (info addOptions (progDesc Help.addTask))
-    addOptions :: Parser SubCommand
-    addOptions =
-      AddTask <$> taskContextOption <*> many (textArgument (help Help.taskText))
-    deleteTaskCommand :: Mod CommandFields SubCommand
-    deleteTaskCommand =
-      command "delete" (info deleteOptions (progDesc Help.deleteTask))
-    deleteOptions :: Parser SubCommand
-    deleteOptions = DeleteTask <$> taskIdArgument
-    checkTaskCommand :: Mod CommandFields SubCommand
-    checkTaskCommand =
-      command "check" (info checkOptions (progDesc Help.checkTask))
-    checkOptions :: Parser SubCommand
-    checkOptions = CheckTask <$> taskIdArgument
-    cancelTaskCommand :: Mod CommandFields SubCommand
-    cancelTaskCommand =
-      command "cancel" (info cancelOptions (progDesc Help.cancelTask))
-    cancelOptions :: Parser SubCommand
-    cancelOptions = CancelTask <$> taskIdArgument
-    startTaskCommand :: Mod CommandFields SubCommand
-    startTaskCommand =
-      command "start" (info startOptions (progDesc Help.startTask))
-    startOptions :: Parser SubCommand
-    startOptions = StartTask <$> taskIdArgument
-    pauseTaskCommand :: Mod CommandFields SubCommand
-    pauseTaskCommand =
-      command "pause" (info pauseOptions (progDesc Help.pauseTask))
-    pauseOptions :: Parser SubCommand
-    pauseOptions = PauseTask <$> taskIdArgument
-    updateTaskTextCommand :: Mod CommandFields SubCommand
-    updateTaskTextCommand =
-      command "update" (info updateTextOptions (progDesc Help.updateTask))
-    updateTextOptions :: Parser SubCommand
-    updateTextOptions =
-      Update <$> taskIdArgument <*> many (textArgument (help Help.taskText))
-    moveTaskCommand :: Mod CommandFields SubCommand
-    moveTaskCommand =
-      command "move" (info moveTaskOptions (progDesc Help.moveTask))
-    moveTaskOptions :: Parser SubCommand
-    moveTaskOptions =
-      Move <$> taskIdArgument <*> textArgument (help Help.contextMove)
-    clearCommand :: Mod CommandFields SubCommand
-    clearCommand =
-      command "clear" (info (pure Clear) (progDesc Help.clearTasks))
-    todayCommand :: Mod CommandFields SubCommand
-    todayCommand = command "today" (info todayOptions (progDesc Help.today))
-    todayOptions :: Parser SubCommand
-    todayOptions = Today <$> contextFilterOption
-    outForTodayCommand :: Mod CommandFields SubCommand
-    outForTodayCommand =
-      command
-        "out-for-today"
-        (info outForTodayOptions (progDesc Help.outForToday))
+
+taskManagementCommands :: Mod CommandFields SubCommand
+taskManagementCommands =
+  commandGroup "Task management:" <> listTasksCommand <> addTaskCommand <>
+  deleteTaskCommand <>
+  checkTaskCommand <>
+  cancelTaskCommand <>
+  startTaskCommand <>
+  pauseTaskCommand <>
+  updateTaskTextCommand <>
+  moveTaskCommand <>
+  clearCommand
+
+reporterCommands :: Mod CommandFields SubCommand
+reporterCommands =
+  commandGroup "Reporters:" <> hidden <> todayCommand <> outForTodayCommand
+
+refManagementCommands :: Mod CommandFields SubCommand
+refManagementCommands =
+  commandGroup "Refs management:" <> hidden <> listRefsCommand <> addRefCommand <>
+  deleteRefCommand
+
+taskFilePathOption :: Parser FilePath
+taskFilePathOption =
+  strOption
+    (long "taskfile" <> short 'f' <> metavar "TASKFILE" <>
+     value Taskfile.defaultPath <>
+     showDefault <>
+     help Help.taskfile)
+
+taskContextOption :: Parser Text
+taskContextOption =
+  textOption
+    (long "context" <> short 'c' <> value Tasks.defaultContext <>
+     help Help.contextFilter)
+
+contextFilterOption :: Parser Ui.ContextFilter
+contextFilterOption = includeContextFilterOption <|> excludeContextFilterOption
+  where
+    includeContextFilterOption =
+      option
+        includeReader
+        (long "include-context" <> short 'i' <> value Ui.All <>
+         help Help.includeContextFilter)
+    includeReader = eitherReader (Right . Ui.Include . pack)
+    excludeContextFilterOption =
+      option
+        excludeReader
+        (long "exclude-context" <> short 'e' <> value Ui.All <>
+         help Help.excludeContextFilter)
+    excludeReader = eitherReader (Right . Ui.Exclude . pack)
+
+taskIdArgument :: Parser Int
+taskIdArgument = argument auto (help Help.taskId)
+
+listTasksCommand :: Mod CommandFields SubCommand
+listTasksCommand =
+  command "list" (info listTasksOptions (progDesc Help.listTasks))
+
+listTasksOptions :: Parser SubCommand
+listTasksOptions = ListTasks <$> contextFilterOption
+
+addTaskCommand :: Mod CommandFields SubCommand
+addTaskCommand = command "add" (info addOptions (progDesc Help.addTask))
+
+addOptions :: Parser SubCommand
+addOptions =
+  AddTask <$> taskContextOption <*> many (textArgument (help Help.taskText))
+
+deleteTaskCommand :: Mod CommandFields SubCommand
+deleteTaskCommand =
+  command "delete" (info deleteOptions (progDesc Help.deleteTask))
+
+deleteOptions :: Parser SubCommand
+deleteOptions = DeleteTask <$> taskIdArgument
+
+checkTaskCommand :: Mod CommandFields SubCommand
+checkTaskCommand = command "check" (info checkOptions (progDesc Help.checkTask))
+
+checkOptions :: Parser SubCommand
+checkOptions = CheckTask <$> taskIdArgument
+
+cancelTaskCommand :: Mod CommandFields SubCommand
+cancelTaskCommand =
+  command "cancel" (info cancelOptions (progDesc Help.cancelTask))
+
+cancelOptions :: Parser SubCommand
+cancelOptions = CancelTask <$> taskIdArgument
+
+startTaskCommand :: Mod CommandFields SubCommand
+startTaskCommand = command "start" (info startOptions (progDesc Help.startTask))
+
+startOptions :: Parser SubCommand
+startOptions = StartTask <$> taskIdArgument
+
+pauseTaskCommand :: Mod CommandFields SubCommand
+pauseTaskCommand = command "pause" (info pauseOptions (progDesc Help.pauseTask))
+
+pauseOptions :: Parser SubCommand
+pauseOptions = PauseTask <$> taskIdArgument
+
+updateTaskTextCommand :: Mod CommandFields SubCommand
+updateTaskTextCommand =
+  command "update" (info updateTextOptions (progDesc Help.updateTask))
+
+updateTextOptions :: Parser SubCommand
+updateTextOptions =
+  Update <$> taskIdArgument <*> many (textArgument (help Help.taskText))
+
+moveTaskCommand :: Mod CommandFields SubCommand
+moveTaskCommand = command "move" (info moveTaskOptions (progDesc Help.moveTask))
+
+moveTaskOptions :: Parser SubCommand
+moveTaskOptions =
+  Move <$> taskIdArgument <*> textArgument (help Help.contextMove)
+
+clearCommand :: Mod CommandFields SubCommand
+clearCommand = command "clear" (info (pure Clear) (progDesc Help.clearTasks))
+
+todayCommand :: Mod CommandFields SubCommand
+todayCommand = command "today" (info todayOptions (progDesc Help.today))
+
+todayOptions :: Parser SubCommand
+todayOptions = Today <$> contextFilterOption
+
+outForTodayCommand :: Mod CommandFields SubCommand
+outForTodayCommand =
+  command "out-for-today" (info outForTodayOptions (progDesc Help.outForToday))
+  where
     outForTodayOptions :: Parser SubCommand
     outForTodayOptions = OutForToday <$> contextFilterOption
-    listRefsCommand :: Mod CommandFields SubCommand
-    listRefsCommand =
-      command "refs" (info (pure ListRefs) (progDesc Help.listRefs))
-    addRefCommand :: Mod CommandFields SubCommand
-    addRefCommand =
-      command "set-ref" (info addRefOptions (progDesc Help.addRef))
-    addRefOptions :: Parser SubCommand
-    addRefOptions =
-      AddRef <$> textArgument (help Help.refService) <*>
-      textArgument (help Help.refUrlTemplate)
-    deleteRefCommand :: Mod CommandFields SubCommand
-    deleteRefCommand =
-      command "delete-ref" (info deleteRefOptions (progDesc Help.deleteRef))
-    deleteRefOptions :: Parser SubCommand
-    deleteRefOptions = DeleteRef <$> textArgument (help Help.refRepoAction)
+
+listRefsCommand :: Mod CommandFields SubCommand
+listRefsCommand = command "refs" (info (pure ListRefs) (progDesc Help.listRefs))
+
+addRefCommand :: Mod CommandFields SubCommand
+addRefCommand = command "set-ref" (info addRefOptions (progDesc Help.addRef))
+
+addRefOptions :: Parser SubCommand
+addRefOptions =
+  AddRef <$> textArgument (help Help.refService) <*>
+  textArgument (help Help.refUrlTemplate)
+
+deleteRefCommand :: Mod CommandFields SubCommand
+deleteRefCommand =
+  command "delete-ref" (info deleteRefOptions (progDesc Help.deleteRef))
+
+deleteRefOptions :: Parser SubCommand
+deleteRefOptions = DeleteRef <$> textArgument (help Help.refRepoAction)
 
 update ::
      SubCommand
@@ -233,7 +249,7 @@ view sc currentTime taskfile =
     AddRef _repo _repoPath -> Ui.showRefs (Taskfile.refs taskfile)
     DeleteRef _repo -> Ui.showRefs (Taskfile.refs taskfile)
     ListTasks contextFilter -> Ui.showTasks contextFilter taskfile currentTime
-    other -> Ui.showTasks Ui.All taskfile currentTime
+    _ -> Ui.showTasks Ui.All taskfile currentTime
 
 executeCommand :: IO ()
 executeCommand = do
@@ -251,3 +267,17 @@ executeCommand = do
           Taskfile.create resolvedTaskFilePath newTaskfile
           view (subCommand opts) currentTime newTaskfile
         Left err -> Ui.showError err
+
+------------------------------------------------------------
+-- Arguably these should go in a file called `Options.Applicative.Extra`.
+------------------------------------------------------------
+textArgument :: Mod ArgumentFields String -> Parser Text
+textArgument = fmap pack . strArgument
+
+textOption :: Mod OptionFields String -> Parser Text
+textOption = fmap pack . strOption
+
+maybeTextOption :: Mod OptionFields (Maybe Text) -> Parser (Maybe Text)
+maybeTextOption = option maybeText
+  where
+    maybeText = eitherReader (Right . Just . pack)
